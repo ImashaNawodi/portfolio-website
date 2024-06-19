0d5cf2d8ee455283d48f980609ebbe94ea00898e
ImashaNawodi <imashanaw1999@gmail.com>
2024-06-19 21:41:29 +0530

    updated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/Imasha-Nawodi-Resume.pdf"
+              href="/Imasha Nawodi-Resume-Software Engineer-Intern.pdf"
               download="Imasha-Nawodi-Resume.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
